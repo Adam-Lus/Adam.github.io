3a98734673e9b79a2d29bffc443d324dd4ffabec
Adam-Lus <576993679@qq.com>
2025-07-05 17:05:52 +0800

    Add audio folder

---
+++ audio/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7db2a97acc2cb46a84418f3f447d8c6b9cdb526481999e5718c832412e5570e
+size 16
---
+++ audio/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf53316179c10f596c3aaeecd5fc215e781006c2ca6dfb179aa0cd4ded7d5127
+size 24767
